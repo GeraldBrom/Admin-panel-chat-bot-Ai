1ec6dd3e70835de6f17fcef60da64a61fcd7be1d
Ivan <lavlisnkiy.i@mail.ru>
2025-10-31 17:27:40 +0300

    Удалил закомментированный код и сообщения о создании пользователей в UserSeeder.php для упрощения кода и улучшения читаемости.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,24 +20,6 @@ class UserSeeder extends Seeder
             'password' => Hash::make('admin123'),
             'email_verified_at' => now(),
         ]);
-
-        // Можно добавить дополнительных пользователей
-        User::create([
-            'name' => 'Администратор-бот',
-            'email' => 'admin-bot@admin.com',
-            'password' => Hash::make('admin-bot123'),
-            'email_verified_at' => now(),
-        ]);
-
-        $this->command->info('✅ Пользователи созданы успешно!');
-        $this->command->info('');
-        $this->command->info('📧 Администратор:');
-        $this->command->info('   Email: admin@admin.com');
-        $this->command->info('   Пароль: admin123');
-        $this->command->info('');
-        $this->command->info('📧 Менеджер:');
-        $this->command->info('   Email: manager@admin.com');
-        $this->command->info('   Пароль: manager123');
     }
 }
 
